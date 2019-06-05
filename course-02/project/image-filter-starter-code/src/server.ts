5889c7800ab051a13808546bb6f2b6791ac8d27f
Emil Martinov <martinov.emil@gmail.com>
2019-06-06 01:55:05 +0200

    Comments edited and new ScreenShot provided.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -16,21 +16,6 @@ import { join } from "path";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
   app.get("/filteredimage", async (req: Request, res: Response) => {
     const image_url: string = req.query.image_url;
     if (!image_url) {
@@ -39,19 +24,21 @@ import { join } from "path";
     const filteredImage = await filterImageFromURL(image_url);
     res.status(200).sendFile(filteredImage);
     const tmp = join(__dirname + "/util/tmp/");
+
+    // get all the files in the directory
     fs.readdir(tmp, async (err, files) => {
       if (err) {
         console.log(err);
       }
+      //create an array of the files' paths.
       const toBeDeleted = files.map(file => {
         console.log(`${file} is deleted`);
         return join(tmp + file);
       });
+      // delete all the files with every request.
       deleteLocalFiles(toBeDeleted);
     });
   });
-
-  //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user
